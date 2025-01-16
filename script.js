4e7877b6e3d409e6ff16567caf522c42ae259571
cactipapa <74386189+ajm19826@users.noreply.github.com>
2025-01-16 17:12:41 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,98 +1,57 @@
-    document.getElementById("openPopup").addEventListener("click", function () {
-      // Open a new window or tab
-      window.open(
-        "https://ajm19826.github.io/home", // URL of the new window
-        "_blank",                 // Target (_blank, _self, etc.)
-        "width=600,height=400"    // Window features (size, etc.)
-      );
-    });
+document.getElementById("openPopup").addEventListener("click", function () {
+  window.open("https://ajm19826.github.io/home", "_blank", "width=600,height=400");
+});
+
+function passWord() {
+  let attempts = 0;
+  const maxAttempts = 3;
+
+  while (attempts < maxAttempts) {
+    const password = prompt("Please Enter Your Password:", "").toLowerCase();
+    if (password === "letmein") {
+      alert("We will redirect you shortly...");
+      window.open("你好.html");
+      return;
+    } else {
+      alert("Access Denied - Password Incorrect. Try Again.");
+      attempts++;
+    }
+  }
+
+  if (attempts === maxAttempts) {
+    alert("Too many failed attempts!");
+    window.history.back();
+  }
+}
+
+// Replace document.write calls with DOM manipulations
+function loadIframe(src, title) {
+  const iframe = document.createElement("iframe");
+  iframe.src = src;
+  iframe.style.width = "100%";
+  iframe.style.height = "100%";
+  iframe.frameBorder = "0";
+  iframe.allowFullscreen = true;
+
+  const backButton = document.createElement("button");
+  backButton.textContent = "Back";
+  backButton.onclick = () => location.reload();
+
+  document.body.innerHTML = ""; // Clear the page
+  document.body.appendChild(iframe);
+  document.body.appendChild(backButton);
+
+  document.title = title || "SmartSchool";
+}
+
 function gdash() {
-  document.write("<iframe src='https://geometrydashonline.io' allowtransparency='true' width='100%' height='100%' frameborder='0' scrolling='yes' allowfullscreen></iframe> <button onclick='window.reload()'>Back</button> ")
+  loadIframe("https://geometrydashonline.io", "Geometry Dash");
 }
-function info() {
-  document.write("<iframe src='info.txt' allowtransparency='true' width='100%' height='100%' frameborder='0' scrolling='no' allowfullscreen></iframe><button onclick='window.reload()'>Back</button> <p>Information Of SmartSchool</p>")
-}
+
 function lessons() {
-  document.write("<iframe src='lesson.html' allowtransparency='true' width='100%' height='100%' frameborder='0' scrolling='no' allowfullscreen></iframe><button onclick='window.reload()'>Back</button> ")
+  loadIframe("lesson.html", "Lessons");
 }
-function turnedin() {
-  alert("Turned In!")
-  window.open("index.html")
-  window.close()
-  document.title("SmartSchool | Turned In")
-}
-function gspot() {
-  document.write("<iframe src='https://geometryspot.com' allowtransparency='true' width='100%' height='100%' frameborder='0' scrolling='yes' allowfullscreen></iframe> <button onclick='window.reload()'>Back</button> ")
-}
+
 function minecraft() {
-  //MINECRAFT
-  document.write("<iframe src='https://eaglecraft.pages.dev' allowtransparency='true' width='100%' height='100%' frameborder='0' scrolling='yes' allowfullscreen></iframe> <button onclick='window.reload()'>Back</button> ")
-}
-function sus() {
-  document.write("<iframe src='https://among-us-1tim-temp-dmain.netlify.app' allowtransparency='true' width='100%' height='100%' frameborder='0' scrolling='yes' allowfullscreen></iframe> <button onclick='window.reload()'>Back</button> ");
-}
-function linerider() {
-  document.write("<iframe src='https://linerider.com' allowtransparency='true' width='100%' height='100%' frameborder='0' scrolling='yes' allowfullscreen></iframe> <button onclick='window.reload()'>Back</button> ");
-}
-function roblox() {
-  document.write("<iframe src='https://roblox.com' allowtransparency='true' width='100%' height='100%' frameborder='0' scrolling='yes' allowfullscreen></iframe> <button onclick='window.reload()'>Back</button> ");
-}
-function takedown() { alert("This Website Is Currently Under Maintenance" + "\n" + "Please Try Again Later") }
-function subway_surfers() {
-  document.write("<iframe src='https://dddavit.github.io/subway/' allowtransparency='true' width='100%' height='100%' frameborder='0' scrolling='yes' allowfullscreen></iframe> <button onclick='window.reload()'>Back</button> ");
-}
-function yohoho() {
-  document.write("<iframe src='https://yohoho.io' allowtransparency='true' width='100%' height='100%' frameborder='0' scrolling='yes' allowfullscreen></iframe> <button onclick='window.reload()'>Back</button> ");
-}
-//Gaming Websites
-
-function monkey() {
-  document.write("<iframe src='https://monkeygg2.github.io' allowtransparency='true' width='100%' height='100%' frameborder='0' scrolling='yes' allowfullscreen></iframe> <button onclick='window.reload()'>Back</button> ");
-}
-}
-function highway() {
-  document.write("<iframe src='https://highwayracer.github.io' allowtransparency='true' width='100%' height='100%' frameborder='0' scrolling='yes' allowfullscreen></iframe> <button onclick='window.reload()'>Back</button> ");
-}
-}
-function poki() {
-  document.write("<iframe src='' allowtransparency='true' width='100%' height='100%' frameborder='0' scrolling='yes' allowfullscreen></iframe> <button onclick='window.reload()'>Back</button> ");
-}
-}
-function monster() {
-  document.write("<iframe src='https://monstertracks.github.io' allowtransparency='true' width='100%' height='100%' frameborder='0' scrolling='yes' allowfullscreen></iframe> <button onclick='window.reload()'>Back</button> ");
-}
-}
-function eggy() {
-  document.write("<iframe src='https://eggycar.github.io' allowtransparency='true' width='100%' height='100%' frameborder='0' scrolling='yes' allowfullscreen></iframe> <button onclick='window.reload()'>Back</button> ");
-}
-
-//Other things
-function houseofhazards() {
-  document.write("<iframe src='https://houseofhazards.com' allowtransparency='true' width='100%' height='100%' frameborder='0' scrolling='yes' allowfullscreen></iframe> <button onclick='window.reload()'>Back</button> ");
-}
-function paper_io() {
-  document.write("<iframe src='https://paper-io.com/' allowtransparency='true' width='100%' height='100%' frameborder='0' scrolling='yes' allowfullscreen></iframe> <button onclick='window.reload()'>Back</button> ");
-}
-function flyufo() {
-  document.write("<iframe src='https://flyufo.io/' allowtransparency='true' width='100%' height='100%' frameborder='0' scrolling='yes' allowfullscreen></iframe> <button onclick='window.reload()'>Back</button> ");
-}
-function paperanimals() {
-  document.write("<iframe src='https://paperanimals.io/' allowtransparency='true' width='100%' height='100%' frameborder='0' scrolling='yes' allowfullscreen></iframe> <button onclick='window.reload()'>Back</button> ");
-}
-function bitlife() {
-  document.write("<iframe src='https://bitlifeonline.io/' allowtransparency='true' width='100%' height='100%' frameborder='0' scrolling='yes' allowfullscreen></iframe> <button onclick='window.reload()'>Back</button> ");
-}
-function stickman_hook() {
-  document.write("<iframe src='https://www.pomu.com/embed/stickman-hook' allowtransparency='true' width='100%' height='100%' frameborder='0' scrolling='yes' allowfullscreen></iframe> <button onclick='window.reload()'>Back</button> ");
-}
-
-function Blanche() {
-  document.write("<HTML><style>#backbutton{float:left;}</style><head><title>SmartSchool | Mrs. Blanche's Page</title><link rel='stylesheet' href='https://www.w3schools.com/w3css/4/w3.css'></head><center><div><button id='backbutton'>Back</button><h1>Mrs. Blanche's Page</h1><br><hr color='black'></center></div><h1>Mrs Blanche has disabled Page Viewing. <b>Reason: Teachers Page</b></h1></HTML>")
-}
-function Compton() {
-  document.write("<HTML><style>.leftpost{float:left;width:10.5cm;height:7.2cm;border-radius: 40px;background-color: lightgray;font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans serif;} .rightpost{float:right;width:4.5cm;height:7.5cm;border-radius: 40px background-color:lightgray;font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans serif;} #backbutton{float:left;}</style><head><title>SmartSchool | Mrs. Compton's Page</title><link rel='stylesheet' href='https://www.w3schools.com/w3css/4/w3.css'></head><center><div><button id='backbutton'>Back</button><h1>Mrs. Compton's Page</h1><br><div class='leftpost'><h3>Today's Agenda</h3><br><li>Reading:<b>20minutes / Book club assigned reading</b></li><li>Writing:<b>Finish Writing Essay</b></li><li>Coding: <b>N/A</b></li><li>Math:<b>Finish Lesson 58</b></li><li>Spelling: <b>N/A</b></li></div></center></div><div class='rightpost'><input type='text' id='Send' placeholder='Mrs. Compton has disabled chatting'> <button disabled onclick='document.GetElementById('Send').innerHTML = ' ' '>Send</button></div> <br><hr color='black'></HTML>")
-}
-function comptonclass() {
-
-  document.write("<html><link rel='stylesheet' href='style.css'><title>SmartSchool | Mrs.Comptons Class</title><style>#main{width:100%; height:6.5cm; background-color:lightblue;}</style><div id='main'><center><h2 style='color:black; font-family:sans-serif;position: relative;bottom:0px;'>Compton, Ms. K._Grade 4_(23-24)-Sec-118289</h2></center></div></html>")
+  loadIframe("https://eaglecraft.pages.dev", "Minecraft");
 }
